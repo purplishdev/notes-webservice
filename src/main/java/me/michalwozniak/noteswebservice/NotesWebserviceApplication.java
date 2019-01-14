d57f8a7b8824496b94654706950425d57c5e26fc
Michał Woźniak <40491049+purplishdev@users.noreply.github.com>
2019-01-14 21:14:25 +0100

    feat(project): Generate project with Spring Initializr

---
+++ src/main/java/me/michalwozniak/noteswebservice/NotesWebserviceApplication.java
@@ -0,0 +1,12 @@
+package me.michalwozniak.noteswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesWebserviceApplication.class, args);
+	}
+}
